607bd5784903f3b5bacbcd3c5d19728ec662b5e5
dsaeun <daeun84366@gmail.com>
2022-06-27 14:18:06 +0900

    Refactor: image atom 크기 조절 레벨 조정

--- src/components/atoms/image/image.tsx
+++ src/components/atoms/image/image.tsx
@@ -5,7 +5,8 @@ export interface ImgProps {
 }
 
 const Img = styled.img`
-  width: 500px;
+  width: 100%;
+  height: 100%;
 `
 
 const image = ({ imgPath }: ImgProps) => {
--- src/components/molecules/introImg/introImg.tsx
+++ src/components/molecules/introImg/introImg.tsx
@@ -3,25 +3,26 @@ import styled from 'styled-components'
 import Image, { ImgProps } from '../../atoms/image/image'
 import Text, { TextProps } from '../../atoms/text/text'
 
+interface IntroImgProps extends TextProps, ImgProps {}
+
 const IntroContainer = styled.div`
   position: relative;
   width: 400px;
   height: 338px;
 `
 const TextContainer = styled.div`
+  width: 100%;
   position: absolute;
-  left: 50%;
-  bottom: -30px;
+  text-align: center;
+  bottom: 45px;
 `
-
-interface IntroImgProps extends TextProps, ImgProps {}
 
 const IntroImg = ({ imgPath, desc }: IntroImgProps) => {
   return (
     <IntroContainer>
       <Image imgPath={imgPath} />
       <TextContainer>
-        <Text desc={desc} fontColor="grey" fontSize="15px" />
+        <Text desc={desc} fontColor="rgba(58, 58, 58, 0.7)" fontSize="15px" />
       </TextContainer>
     </IntroContainer>
   )
