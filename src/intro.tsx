73170f9aa42f7d5f3789c77cc71998495e84e553
dsaeun <daeun84366@gmail.com>
2022-06-27 15:45:15 +0900

    Feat: page 컴포넌트 종합 렌더링 data를 static으로 선언 해두고, props로 내림. 추 후 경우에 따라 리팩토링 필요.

--- src/intro.tsx
+++ src/intro.tsx
@@ -1,9 +1,54 @@
-function intro() {
+import styled from 'styled-components'
+
+import IntroModule from './components/organisms/intro/introModule'
+import logo from './asset/img/triple2x.png'
+import playstore from './asset/img/play-store2x.png'
+import apple from './asset/img/badge-apple4x.png'
+
+const Container = styled.div`
+  width: 1200px;
+`
+
+const Intro = () => {
+  const data = {
+    introImgInfo: {
+      imgPath: logo,
+      desc: '2019년 2월 기준',
+    },
+    awards: [
+      {
+        imgPath: playstore,
+        desc: (
+          <>
+            2018 구글 플레이스토어
+            <br />
+            올해의 앱 최우수상 수상
+          </>
+        ),
+      },
+      {
+        imgPath: apple,
+        desc: (
+          <>
+            2018 애플 앱스토어
+            <br />
+            오늘의 여행 앱 선정
+          </>
+        ),
+      },
+    ],
+    indecators: ['100만명의 사용자', '101만개의 리뷰', '102만명의 사용자'],
+  }
+
   return (
-    <div>
-      <div>home</div>
-    </div>
+    <Container>
+      <IntroModule
+        introImgInfo={data.introImgInfo}
+        awards={data.awards}
+        indecators={data.indecators}
+      />
+    </Container>
   )
 }
 
-export default intro
+export default Intro
